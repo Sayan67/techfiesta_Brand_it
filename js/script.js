107a4dd55e9b1268ab439b99165e945427955aff
Sayan67 <sayan.das.21@aot.edu.in>
2024-04-08 17:37:57 +0530

    added functionality

--- js/script.js
+++ js/script.js
@@ -1,39 +1,97 @@
+// var totalBooks = 0;
+// var totalIssuedBooks =0;
+// document.getElementById('totalBooks').innerHTML = "Total Books :"+localStorage.getItem("totalBooks");
+// document.getElementById('allIssuedBooks').innerHTML = "Issued Books :"+localStorage.getItem("totalIssues");
 
-var totalBooks =0 
-var totalIssuedBooks = 0
+// localStorage.setItem("totalBooks", totalBooks);
+// localStorage.setItem("totalIssues", totalIssuedBooks);
 
-document.getElementById('totalBooks').innerHTML = "Total Books :"+localStorage.getItem("totalBooks");
-document.getElementById('issuedBooks').innerHTML = "Issued Books :"+localStorage.getItem("totalIssues");
-
-document.getElementById("myForm").addEventListener("submit", function(event) {
-    event.preventDefault(); 
-    let formData = new FormData(this);
+// document.getElementById("myForm").addEventListener("submit", function(event) {
+//     event.preventDefault(); 
+//     let formData = new FormData(this);
   
-    let formDataObj = {};
-    formData.forEach(function(value, key){
-      formDataObj[key] = value;
-    });
-    totalBooks = formDataObj.books;
-    localStorage.setItem("totalBooks", totalBooks);
-    document.getElementById('totalBooks').innerHTML = "Total Books :"+totalBooks;
+//     let formDataObj = {};
+//     formData.forEach(function(value, key){
+//       formDataObj[key] = value;
+//     });
+//     totalBooks = parseInt(formDataObj.books);
+//     localStorage.setItem("totalBooks", totalBooks);
+//     document.getElementById('totalBooks').innerHTML = "Total Books :"+totalBooks- totalIssuedBooks;
 
-  });
+//   });
 
 
-  document.getElementById("issuedBooks").addEventListener("submit", function(event) {
-    event.preventDefault(); 
-    let data = new FormData(this);
+//   var numOfIssues = document.getElementById('issuedBooks').addEventListener('submit',function(){
+//     totalIssuedBooks+=1;
+//     totalBooks = parseInt(localStorage.getItem("totalBooks"))-totalIssuedBooks;
+//     console.log(totalIssuedBooks);
+//     localStorage.setItem("totalIssues", totalIssuedBooks);
+//     document.getElementById('allIssuedBooks').innerHTML = "Issued Books :"+(totalIssuedBooks) ;
+//     document.getElementById('totalBooks').innerHTML = "Total Books :"+(totalBooks) ;
+//     localStorage.setItem("totalBooks", totalBooks);
+//   })
+
+
+
+// //   document.getElementById("issuedBooks").addEventListener("submit", function(event) {
+// //     event.preventDefault(); 
+// //     let data = new FormData(this);
   
-    let allIssues = {};
-    data.forEach(function(value, key){
-        allIssues[key] = value;
-    });
-    totalIssuedBooks = allIssues.books;
-    console.log("hello");
-    console.log(allIssues.issue);
-    localStorage.setItem("totalIssues", totalIssuedBooks);
-    document.getElementById('totalBooks').innerHTML = "Total Books :"+totalIssuedBooks;
+// //     let allIssues = {};
+// //     data.forEach(function(value, key){
+// //         allIssues[key] = value;
+// //     });
+// //     totalIssuedBooks = allIssues.issue;
+// //     console.log("hello");
+// //     console.log(allIssues.issue);
+// //     localStorage.setItem("totalIssues", totalIssuedBooks);
+// //     document.getElementById('totalBooks').innerHTML = "Total Books :"+totalIssuedBooks;
 
-  });
+// //   });
 
 
+var totalBooks = localStorage.getItem("totalBooks") || 0;
+        var totalIssuedBooks = localStorage.getItem("totalIssues") || 0;
+
+        document.getElementById('totalBooks').innerHTML = "Total Books : " + totalBooks;
+        document.getElementById('issuedBooks').innerHTML = "Issued Books : " + totalIssuedBooks;
+
+        document.getElementById("myForm").addEventListener("submit", function(event) {
+            event.preventDefault(); 
+            let formData = new FormData(this);
+          
+            let formDataObj = {};
+            formData.forEach(function(value, key){
+              formDataObj[key] = value;
+            });
+            totalBooks = formDataObj.books;
+            localStorage.setItem("totalBooks", totalBooks);
+            document.getElementById('totalBooks').innerHTML = "Total Books : " + totalBooks;
+        });
+
+        document.getElementById("issuedBooksForm").addEventListener("submit", function(event) {
+            event.preventDefault(); 
+            let data = new FormData(this);
+          
+            let allIssues = {};
+            data.forEach(function(value, key){
+                allIssues[key] = value;
+            });
+            let issuedCount = allIssues.issue;
+            if(issuedCount>totalBooks){
+                alert("Not possible");
+                return;
+            }
+            if(totalBooks!=0){
+
+                totalIssuedBooks = parseInt(totalIssuedBooks) + parseInt(issuedCount);
+            }
+            if(totalBooks>0){
+
+                totalBooks -= issuedCount;
+            }
+            localStorage.setItem("totalIssues", totalIssuedBooks);
+            localStorage.setItem("totalBooks", totalBooks);
+            document.getElementById('issuedBooks').innerHTML = "Issued Books : " + totalIssuedBooks;
+            document.getElementById('totalBooks').innerHTML = "Total Books : " + totalBooks;
+        });
